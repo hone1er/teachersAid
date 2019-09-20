64e4c124fcbd830c05bd08efb3fc956a7d31ec74
hone1er <josephmvillavicencio@gmail.com>
2019-09-20 12:31:10 -0700

    work around for illuminateED implemented

--- autodojo.py
+++ autodojo.py
@@ -1,8 +1,10 @@
+#! /user/local/bin/python3
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup as soup
 import os
 import time
@@ -16,15 +18,19 @@ class Bot:
         self.scores = {}
         self.score_dict = {}
         self.periods = None
+        self.date_xpath = None
         print("INITIALIZING")
+        self.urls = ['https://berkeley.illuminateed.com/dna/?gradebook_id=8742&page=GradebookScoresheet', 'https://berkeley.illuminateed.com/dna/?gradebook_id=8632&view=scoresheet&page=GradebookScoresheet',
+                     'https://berkeley.illuminateed.com/dna/?gradebook_id=8670&view=scoresheet&page=GradebookScoresheet', 'https://berkeley.illuminateed.com/dna/?gradebook_id=8744&view=scoresheet&page=GradebookScoresheet', 'https://berkeley.illuminateed.com/dna/?gradebook_id=8745&view=scoresheet&page=GradebookScoresheet']
 
     def login(self):
         print("LOGGING IN")
         self.driver.get(
             "https://teach.classdojo.com/#/login?redirect=%2Flaunchpad")
         assert "ClassDojo" in self.driver.title
-        username = self.driver.find_element_by_xpath(
-            '//*[@id="reactApplication"]/div/div/div[2]/div/form/div[1]/span/div[1]/div/input')
+        username = WebDriverWait(self.driver, 7).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="reactApplication"]/div/div/div[2]/div/form/div[1]/span/div[1]/div/input')))
         username.click()
         username.send_keys("joshuapaz@berkeley.net")
         password = self.driver.find_element_by_xpath(
@@ -52,14 +58,35 @@ class Bot:
             '//*[@id="reactApplication"]/div/div/div/div/div/div[2]/div[1]/div/div[5]/div/div[3]/span/div/div/div/div/div/ul/li[2]/div/div[1]')
         report.click()
         time.sleep(1)
-        week = self.driver.find_element_by_xpath(
+        date = self.driver.find_element_by_xpath(
             '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[1]/div/div[1]/span/div/a/div/div[1]')
-        week.click()
+        date.click()
+
+    def custom_dates(self):
+        custom_dates = self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[1]/div/div[1]/span/div/div/div/div/div/ul/li[8]/div/div[1]')
+        time.sleep(1)
+        custom_dates.click()
+        time.sleep(1)
+
+    def last_week(self):
         last_week = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[1]/div/div[1]/span/div/div/div/div/div/ul/li[4]/div/div[1]')
+            '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[1]/div/div[1]/span/div/div/div/div/div/ul/li[8]/div/div[1]')
         time.sleep(1)
         last_week.click()
         time.sleep(1)
+
+    def check_exists_by_xpath(self, xpath):
+        try:
+            self.driver.find_elements_by_link_text(xpath)
+        except NoSuchElementException:
+            self.date_xpath = False
+        self.date_xpath = True
+
+    def choose_dates(self):
+        if input(f"Have you chosen the dates for this period?('y'/'n') ").lower() == 'y':
+            return
+        self.choose_dates()
 
     def get_scores(self):
         print("COLLECTING SCORES")
@@ -96,6 +123,11 @@ class Bot:
         self.driver.get(
             "https://berkeley.illuminateed.com/dna/?GradebookSelect")
 
+    def logged_in(self):
+        if input("Have you logged-in to illuminateEd?('y'/'n') ").lower() == 'y':
+            return
+        self.logged_in()
+
     def login_w_google(self):
         google_login = WebDriverWait(self.driver, 7).until(
             EC.presence_of_element_located(
@@ -114,7 +146,7 @@ class Bot:
         next_button.click()
 
     def enter_password(self):
-        password = WebDriverWait(self.driver, 7).until(
+        password = WebDriverWait(self.driver, 27).until(
             EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')))
         password.send_keys("J@m1169o")
@@ -122,6 +154,19 @@ class Bot:
             EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="passwordNext"]/span')))
         next_button.click()
+    
+    def score_mode(self):
+        xpath = '//*[@id="canvas"]/form[1]/table[2]/tbody/tr/td[1]/select'
+        menu = WebDriverWait(self.driver, 7).until(
+            EC.presence_of_element_located(
+                (By.XPATH, xpath)))
+        for option in menu.find_elements_by_tag_name('option'):
+            if option.text == 'Score':
+                option.click() # select() in earlier versions of webdriver
+                break
+
+
+
 
 
 if __name__ == '__main__':
@@ -135,8 +180,17 @@ if __name__ == '__main__':
         key = periods[i].get_attribute("innerText")[0:9]
         periods[i].click()
         bot.go_to_report()
+        bot.custom_dates()
+        bot.choose_dates()
         bot.get_scores()
         bot.score_dict[key] = bot.scores
         bot.return_to_launchpad()
     for k, v in bot.score_dict.items():
         print(k, v)
+    bot.get_illuminateed()
+    bot.logged_in()
+    for url in bot.urls:
+        bot.driver.get(url)
+        bot.score_mode()
+        time.sleep(2)
+    time.sleep(5000)
